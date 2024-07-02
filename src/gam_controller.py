d41836431eb5276a806d4feb953a25fed653979d
Deepak Chourasia <dchouras@redhat.com>
2024-07-02 13:45:31 +0530

    adding raw changes

---
+++ src/gam_controller.py
@@ -0,0 +1,17 @@
+
+class gam_controller:
+    def __init__(self, business_component:str):
+        self.business_component = business_component
+
+
+    def read_component_details(self):
+        pass
+
+    def read_gam_config(self):
+        pass
+
+    def execute_custom_decider(self):
+        pass
+
+    def post_umb_message(self):
+        pass
